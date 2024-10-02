ac93db06cadcbfc087d3a46899aa783490cc7e26
aleks <aleksei.rvachev@mail.ru>
2024-10-03 00:08:19 +0300

    Add "/start" script

--- src/main/java/ru/leska/telegrambot/config/BotConfig.java
+++ src/main/java/ru/leska/telegrambot/config/BotConfig.java
@@ -1,10 +1,16 @@
 package ru.leska.telegrambot.config;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+@Setter
+@Getter
 @Configuration
+@Data
 @PropertySource("application.properties")
 public class BotConfig {
 
